7d9c53d1883843cfec5283d4c2c977d5c55b742a
dalip <dalipkumar703@gmail.com>
2017-03-09 21:36:29 +0530

    update default.php

--- resources/views/template/default.blade.php
+++ resources/views/template/default.blade.php
@@ -64,7 +64,7 @@
 
 
 	<!-- Modernizr JS -->
-	<script src="public/js/modernizr-2.6.2.min.js"></script>
+	<script src="js/modernizr-2.6.2.min.js"></script>
 	<!-- FOR IE9 below -->
 	<!--[if lt IE 9]>
 	<script src="js/respond.min.js"></script>
